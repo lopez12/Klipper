088133e5bc11bcb704d0f94003e3132c1613c84b
Arturo Lopez <lopez.glez1@gmail.com>
2016-04-01 14:23:00 -0600

    Primer push

---
+++ app/src/main/java/mx/com/makingprojects/klipper/MapsKlipperActivity.java
@@ -0,0 +1,63 @@
+package mx.com.makingprojects.klipper;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapsKlipperActivity extends FragmentActivity implements OnMapReadyCallback {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_klipper);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationProvider);
+
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        /*String total2 = Double.toString(latitude);
+
+        Toast toast = Toast.makeText(getApplicationContext(), total2 ,Toast.LENGTH_SHORT);
+        toast.show();*/
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(latitude, longitude);
+        map.addMarker(new MarkerOptions().position(sydney).title("Here I am"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+}
